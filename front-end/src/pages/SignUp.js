f335aa9a7d0ae4cc039b7833f05ddeaca4e45f0b
Chris Bacon <chrisbacon2009@hotmail.com>
2021-12-18 22:38:39 +0000

    Add auth hooks

--- front-end/src/pages/SignUp.js
+++ front-end/src/pages/SignUp.js
@@ -1,42 +1,53 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useToken } from "../auth/useToken";
+import axios from "axios";
 
 export const SignUpPage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [error, setError] = useState("");
+  const [token, setToken] = useToken();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleSignUpAttempt = async () => {
-        history.push("/");
-    }
+  const handleSignUpAttempt = async () => {
+    const response = await axios.post("/api/signup", {
+      email: email,
+      password: password,
+    });
 
-    return (
-        <div class="content-container">
-            <h1>Sign Up</h1>
-            { error && <div className="fail">{error}</div>}
-            <input
-                type="email"
-                placeholder="email"
-                value={email}
-                onChange={e => setEmail(e)}
-            />
-            <input
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={e => setPassword(e)}
-            />
-            <input
-                type="password"
-                placeholder="password"
-                value={confirmPassword}
-                onChange={e => setConfirmPassword(e)}
-            />
-            <button onClick={handleSignUpAttempt}>Sign Up</button>
-            <button onClick={e => history.push("/login")}>Already have an account? Login</button>
-        </div>
-    )
+    const { token } = response.data;
+    setToken(token);
+
+    history.push("/");
+  }
+
+  return (
+    <div class="content-container">
+      <h1>Sign Up</h1>
+      {error && <div className="fail">{error}</div>}
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={e => setEmail(e)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={e => setPassword(e)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={confirmPassword}
+        onChange={e => setConfirmPassword(e)}
+      />
+      <button onClick={handleSignUpAttempt}>Sign Up</button>
+      <button onClick={e => history.push("/login")}>Already have an account? Login</button>
+    </div>
+  )
 }
